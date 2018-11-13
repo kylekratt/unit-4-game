c1b7d3e0a78b9ff7f3a51b6169e08aba493eba20
Kyle <kylekratt@gmail.com>
2018-11-13 14:17:14 -0500

    Added structure for start page for the game, added objects for each Jedi, formatted css for the initial game

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+var obiwan = {
+    hp=150,
+    attack=6,
+    counter=12
+}
+var yoda = {
+    hp=100,
+    attack=12,
+    counter=16
+}
+var windu = {
+    hp=180,
+    attack=4,
+    counter=12
+}
+var anakin = {
+    hp=120,
+    attack=9,
+    counter=9
+}
