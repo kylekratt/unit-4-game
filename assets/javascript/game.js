c5d335f988505964b2a55495aaf5bea28af7b536
Kyle Kratt <kylekratt@gmail.com>
2018-11-14 01:21:26 -0500

    Added basic code for the initial character select along with animations, started on the structure for the duel

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,38 @@
+
 var obiwan = {
-    hp=150,
-    attack=6,
-    counter=12
+    hp:150,
+    attack:6,
+    counter:12
 }
 var yoda = {
-    hp=100,
-    attack=12,
-    counter=16
+    hp:100,
+    attack:12,
+    counter:16
 }
 var windu = {
-    hp=180,
-    attack=4,
-    counter=12
+    hp:180,
+    attack:4,
+    counter:12
 }
 var anakin = {
-    hp=120,
-    attack=9,
-    counter=9
+    hp:120,
+    attack:9,
+    counter:9
 }
+
+$(document.body).on('click',".Jedi", function(){
+    $(this).removeClass("Jedi").addClass("myChar");
+    $(".Jedi").animate({opacity: "hide"});
+    setTimeout(function(){$(".Jedi").appendTo($("#enemies"))}, 350);
+    $("h2").html("Your Character");
+    setTimeout(function(){$("#charSelect").css("justify-content", "center")}, 350);
+    setTimeout(function(){$(".Jedi").removeClass("Jedi").addClass("enemy")}, 500); 
+    setTimeout(function(){$(".enemy").animate({opacity: "show"})},1000);
+    setTimeout(function(){$("h3").css("visibility", "visible")},1000);
+    console.log("this ran instead");
+})
+$(document.body).on('click',".enemy", function(){
+    console.log("this ran");
+    var opponent = $(this).attr("id");
+    console.log(opponent);
+})
